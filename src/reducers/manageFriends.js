c2955f7e34f6796a76877ce3f7d6ba75a4c16739
Lara Sanders <larasanders@gmail.com>
2019-06-16 15:36:35 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
+  let state = {manageFriends: 0}
+  let friends = {name: 'Avi', hometown: 'NYC', id: 100}
+
 export function manageFriends(state, action){
-}
+
+    switch (action.type) {
+    case 'ADD_FRIEND':
+      return manageFriends, {friends + 1}
+    default:
+      return state;
+  }
+  }
