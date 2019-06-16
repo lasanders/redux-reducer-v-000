500f651b3e4f39241b8b2b010b1ae0ecd479d143
Lara Sanders <larasanders@gmail.com>
2019-06-16 15:42:15 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -5,7 +5,7 @@ export function manageFriends(state, action){
 
     switch (action.type) {
     case 'ADD_FRIEND':
-      return manageFriends, {friends + 1}
+      return manageFriends, {...friends, friends.friend + 1}
     default:
       return state;
   }
